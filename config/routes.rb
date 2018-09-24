a8c825bcec75b4fdd729e5a4033268f487120b50
sabine <drsabinethomas@gmail.com>
2018-09-24 09:51:36 -0700

    Added Styling and need to add complete task button

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,21 @@ Rails.application.routes.draw do
 
 root "tasks#index"
 
-post '/tasks', to: 'tasks#create', as: 'create_task'
-get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+# get 'tasks/', to:'task#index'
 
 get '/tasks/new', to:'tasks#new', as: 'new_task'
-patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
-  # put '/tasks/:id', to: 'tasks#update'
 
-delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+post '/tasks', to: 'tasks#create', as: 'create_task'
+
 get '/tasks/:id', to:'tasks#show', as: 'task'
 
+put '/tasks/:id', to: 'tasks#update'
+
+get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
+
+delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
 #the column id makes a variable, it is a placeholder that can be anything,
 end
 
